b85a3f96a886c854f632bf35604109cb5f058036
ty <3272577027@qq.com>
2023-01-27 13:22:10 +0800

    add test

--- web/src/components/cuteRabbit.vue
+++ web/src/components/cuteRabbit.vue
@@ -2,6 +2,7 @@
   <div>
     <img v-show="flag" class="round" alt="Rabbit" @click="fun" src="../assets/rabbit-1.jpg">
     <img v-show="!flag" class="round" alt="Rabbit" src="../assets/rabbit-2.jpg">
+    <audio src="../assets/hash.mp3"></audio>
     <h1 class="rainbow"> 你戳了可爱兔兔 {{ cnt }} 下</h1>
   </div>
 </template>
