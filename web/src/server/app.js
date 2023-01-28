c1094ca857d91d4b76ddb920f4695e660acbb4b5
ty <3272577027@qq.com>
2023-01-28 14:54:44 +0800

    update server port

--- web/src/server/app.js
+++ web/src/server/app.js
@@ -9,6 +9,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors())              //配置跨域
 app.use(router)              //配置路由
 
-app.listen(80, () => {
+app.listen(1234, () => {
     console.log('success!!!');
 })
