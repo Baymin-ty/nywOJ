9d31ff79c2c9d94f7fe8ffa064786a665bfe7391
ty <3272577027@qq.com>
2023-03-19 16:15:24 +0800

    shorter load time

--- web/vue.config.js
+++ web/vue.config.js
@@ -11,4 +11,41 @@ module.exports = defineConfig({
       }
     }
   },
+  chainWebpack(config) {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.splitChunks({
+        cacheGroups: {
+          common: {
+            name: 'chunk-common',
+            chunks: 'initial',
+            minChunks: 1,
+            maxInitialRequests: 5,
+            minSize: 0,
+            priority: 1,
+            reuseExistingChunk: true
+          },
+          vendors: {
+            name: 'chunk-vendors',
+            test: /[\\/]node_modules[\\/]/,
+            chunks: 'initial',
+            maxSize: 100000,
+            maxInitialRequests: 20,
+            priority: 2,
+            reuseExistingChunk: true,
+            enforce: true
+          },
+          antDesignVue: {
+            name: 'chunk-ant-design-vue',
+            test: /[\\/]node_modules[\\/]ant-design-vue[\\/]/,
+            chunks: 'initial',
+            priority: 3,
+            maxSize: 100000,
+            reuseExistingChunk: true,
+            enforce: true
+          }
+        }
+      })
+    }
+  }
+
 });
